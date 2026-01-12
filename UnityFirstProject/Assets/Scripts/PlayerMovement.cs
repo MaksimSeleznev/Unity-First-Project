d1c799c689e67c97239fdf6ce85b087a08d91dea
MaksimSeleznev <seleznevmaksim748@gmail.com>
2026-01-12 22:16:42 +0500

    Add smooth rotation based on movement direction

--- UnityFirstProject/Assets/Scripts/PlayerMovement.cs
+++ UnityFirstProject/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float rotationSpeed = 10f;
+
     public float speed = 5f;
     private Rigidbody rb;
 
@@ -16,6 +18,16 @@ public class PlayerMovement : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(horizontal, 0f, vertical);
+        if (moveDirection != Vector3.zero)
+{
+    Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+    transform.rotation = Quaternion.Slerp(
+        transform.rotation,
+        targetRotation,
+        rotationSpeed * Time.deltaTime
+    );
+}
+
 
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
